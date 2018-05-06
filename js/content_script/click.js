237f269fb4b7bef4e62f6b40150f057e409c49d2
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-05-06 12:20:27 -0700

    Expanded font

--- js/content_script/click.js
+++ js/content_script/click.js
@@ -1,1 +1,25 @@
-var globalStatusInfo;$(document).ready(function(){if(window.location.href.match(/https:\/\/www.google.com\/recaptcha\/api\d\/anchor/)&&typeof document.referrer!="undefined"){chrome.runtime.sendMessage({type:"getGlobalStatus"},function(globalStatus){globalStatusInfo=globalStatus;if(typeof globalStatusInfo.enable_autoclick!=="undefined"&&globalStatusInfo.enable_autoclick){var parentUrl=window.location!=window.parent.location?document.referrer:document.location.href;var parentUrlParsed=parseUrl(parentUrl);if(typeof globalStatusInfo.autoclick_exclusions!=="object"||!Array.isArray(globalStatusInfo.autoclick_exclusions)||globalStatusInfo.autoclick_exclusions.indexOf(parentUrlParsed.hostname)===-1){var interval=setInterval(function(){if($(".recaptcha-checkbox").length){clearInterval(interval);$(".recaptcha-checkbox").click()}},500);setTimeout(function(){clearInterval(interval)},1e4)}}})}});
+var globalStatusInfo;
+$(document).ready(function() {
+    if (window.location.href.match(/https:\/\/www.google.com\/recaptcha\/api\d\/anchor/) && typeof document.referrer != "undefined") {
+        chrome.runtime.sendMessage({
+            type: "getGlobalStatus"
+        }, function(globalStatus) {
+            globalStatusInfo = globalStatus;
+            if (typeof globalStatusInfo.enable_autoclick !== "undefined" && globalStatusInfo.enable_autoclick) {
+                var parentUrl = window.location != window.parent.location ? document.referrer : document.location.href;
+                var parentUrlParsed = parseUrl(parentUrl);
+                if (typeof globalStatusInfo.autoclick_exclusions !== "object" || !Array.isArray(globalStatusInfo.autoclick_exclusions) || globalStatusInfo.autoclick_exclusions.indexOf(parentUrlParsed.hostname) === -1) {
+                    var interval = setInterval(function() {
+                        if ($(".recaptcha-checkbox").length) {
+                            clearInterval(interval);
+                            $(".recaptcha-checkbox").click()
+                        }
+                    }, 500);
+                    setTimeout(function() {
+                        clearInterval(interval)
+                    }, 1e4)
+                }
+            }
+        })
+    }
+});
--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,12 @@
-function parseUrl(url){var parser=document.createElement("a");parser.href=url;return parser;parser.protocol;parser.hostname;parser.port;parser.pathname;parser.search;parser.hash;parser.host}
+function parseUrl(url) {
+    var parser = document.createElement("a");
+    parser.href = url;
+    return parser;
+    parser.protocol;
+    parser.hostname;
+    parser.port;
+    parser.pathname;
+    parser.search;
+    parser.hash;
+    parser.host
+}
