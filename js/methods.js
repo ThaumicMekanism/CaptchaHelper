ad0889eca9760d27460558489291e0d821c627af
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-05-06 12:13:38 -0700

    Initial commit of v0.3

---
+++ js/methods.js
@@ -0,0 +1,1 @@
+function parseUrl(url){var parser=document.createElement("a");parser.href=url;return parser;parser.protocol;parser.hostname;parser.port;parser.pathname;parser.search;parser.hash;parser.host}
